ef062fb6f991be3587f14ac5078007309686ff7d
kingpoogie <bmadeley@getgoldn.com>
2025-01-03 13:00:55 -0500

    block_to_block_type and markdown_to_blocks added

--- src/code_logic.py
+++ src/code_logic.py
@@ -5,7 +5,8 @@ import re
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     text_node_list = []
     for node in old_nodes:
-        if node.text_type is TextType.TEXT:
+        #print(f"The node: {node}")
+        if node.text_type == TextType.TEXT:
             current_text = node.text
             while True:
                 start_pos = current_text.find(delimiter)
@@ -58,17 +59,15 @@ def split_nodes_link(old_nodes):
 
         node_text = node.text
 
-        while len(links) > 0:
-            #check if links are in the node_text
-            links = extract_markdown_links(node_text)
-            for link_details in links:
-                alt = link_details[0]
-                url = link_details[1]
-                #split the node text at the link
-                split_text = node_text.split(f"[{alt}]({url})", 1)
-                #append split_text[0] to the overall list
-                text_node_list.append(TextNode(split_text[0],TextType.TEXT))
-                text_node_list.append(TextNode(f"{alt}",TextType.LINK,url))
+        #check if links are in the node_text
+        for link_details in links:
+            alt, url = link_details
+            #split the node text at the link
+            split_text = node_text.split(f"[{alt}]({url})", 1)
+            #append split_text[0] to the overall list
+            text_node_list.append(TextNode(split_text[0],TextType.TEXT))
+            text_node_list.append(TextNode(alt,TextType.LINK,url))
+            if len(split_text) == 2:
                 node_text = split_text[1]
         
         if node_text:
@@ -91,19 +90,19 @@ def split_nodes_images(old_nodes):
 
         node_text = node.text
 
-        while len(images) > 0:
-            #check if links are in the node_text
-            images = extract_markdown_links(node_text)
-            for img_details in images:
-                alt = img_details[0]
-                url = img_details[1]
-                #split the node text at the link
-                split_text = node_text.split(f"![{alt}]({url})", 1)
-                #append split_text[0] to the overall list
-                text_node_list.append(TextNode(split_text[0],TextType.TEXT))
-                text_node_list.append(TextNode(f"{alt}",TextType.IMAGE,url))
-                if len(split_text) == 2:
-                    node_text = split_text[1]
+
+        #check if links are in the node_text
+        images = extract_markdown_images(node_text)
+        for img_details in images:
+            alt = img_details[0]
+            url = img_details[1]
+            #split the node text at the link
+            split_text = node_text.split(f"![{alt}]({url})", 1)
+            #append split_text[0] to the overall list
+            text_node_list.append(TextNode(split_text[0],TextType.TEXT))
+            text_node_list.append(TextNode(f"{alt}",TextType.IMAGE,url))
+            if len(split_text) == 2:
+                node_text = split_text[1]
                         
         if node_text:
             text_node_list.append(TextNode(node_text, TextType.TEXT))
@@ -115,12 +114,27 @@ def text_to_textnodes(text):
     #apply all splitting functions to the text
     
     #convert to a textnode
-    text_node = TextNode(text, TextType.TEXT)
-    converted_nodes = split_nodes_delimiter([text_node], "**", TextType.BOLD)
-    converted_nodes = split_nodes_delimiter([converted_nodes], "*", TextType.ITALIC)
-    converted_nodes = split_nodes_delimiter([converted_nodes, "`", TextType.CODE])
-    converted_nodes = split_nodes_images([converted_nodes])
-    converted_nodes = split_nodes_link([converted_nodes])
+    text_node = [TextNode(text, TextType.TEXT)]
+    print("Initial node: ", text_node)
+    converted_nodes = split_nodes_delimiter(text_node, "**", TextType.BOLD)
+    print("After bold: ", converted_nodes)
+    converted_nodes = split_nodes_delimiter(converted_nodes, "*", TextType.ITALIC)
+    print("After italics: ", converted_nodes)
+    converted_nodes = split_nodes_delimiter(converted_nodes, "`", TextType.CODE)
+    print("After code: ", converted_nodes)
+    
+    try: 
+        converted_nodes = split_nodes_images(converted_nodes)
+        print("After images: ", converted_nodes)
+    except Exception as e:
+        print("Error in split_nodes_images: ", e)
+
+    try: 
+        converted_nodes = split_nodes_link(converted_nodes)
+        print("After images: ", converted_nodes)
+    except Exception as e:
+        print("Error in split_nodes_link: ", e)
+    
  
     
 
@@ -132,6 +146,81 @@ def text_to_textnodes(text):
 #print(text_to_textnodes("*this is italic text*"))
 #print(text_to_textnodes("`this is code text`"))
 #print(text_to_textnodes("here is a link: [to google](https://google.com)"))
-#print(text_to_textnodes("and my friend, an image of my dog ![an image](http://mydog.com/dog.jpg)"))
-#print(text_to_textnodes("This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"))
-print(text_to_textnodes("![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg)"))
+#print(text_to_textnodes("and my friend, an image of my dog: ![an image](http://mydog.com/dog.jpg)"))
+#print(text_to_textnodes("This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)."))
+#print(text_to_textnodes("![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg)"))
+
+def markdown_to_blocks(markdown):
+    #take raw markdown string (a full document) as input and returns a list of "block" strings
+    #md blocks are separated by a blank, empty line. 
+    #strip any leading or trailing whitespace from each block
+    #remove empty blocks due to excessive newlines
+
+
+    #separate the string into a list
+    markdown_list = markdown.split("\n\n")
+    filtered_blocks = []
+    for block in markdown_list:
+        #if the block is empty, remove it
+        if block == '':
+            continue
+        #strip any extra leading or trailing whitespace from each block
+        block = block.strip()
+        filtered_blocks.append(block)
+    
+    return markdown_list
+
+def block_to_block_type(md_block):
+    # determine what kind of block md_block is (markdown string)
+    #supports h1-6, code blocks, quote blocks, ul and ol, and normal text
+
+    #look for header start with regex
+    x = re.search(r"^#{1,6}\s",md_block)
+    if x != None:
+        return "heading"
+    #look for 3 backticks at start and end for code blocks
+    if md_block.startswith("```") and md_block.endswith("```"):
+        return "code"
+    #look for > at start of each line for code block
+    split_line = md_block.splitlines()
+    is_quote = False
+    for line in split_line:
+        if line.startswith(">") == False:
+            is_quote = False
+            break
+        is_quote = True
+    if is_quote:
+        return "quote"
+    # check for ordered list
+    is_ol = False
+    is_ul = True
+    for y in range(len(split_line)-1):
+        if split_line[y].startswith(f"{y+1}. "):
+            is_ol = True
+        elif split_line[y].startswith("* ") or split_line[y].startswith("- "):
+            is_ul = True
+        else:
+            is_ol = False
+            is_ul = False
+            break
+    if is_ol:
+        return "ordered_list"
+    if is_ul:
+        return "unordered_list"
+    
+    return "paragraph"
+
+
+h1heading = "# An h1 heading here"
+h2heading = "## This is a h2 heading"
+h3heading = "### This is an h3 heading"
+code_block = "```\nThis is a code block\n```"
+quote_block = ">this is a quote block\n>on two lines"
+unordered_list = "* an unordered list item\n* the second item\n* The third item"
+ordered_list = "1. first item of ordered list\n2. second item and \n3. third item"
+regular_text = "this is just regular paragraph text"
+
+tests = [h1heading, h2heading, h3heading, code_block, quote_block, unordered_list, ordered_list, regular_text]
+
+for test in tests:
+    print(f"\"{test}\" is a {block_to_block_type(test)}")
