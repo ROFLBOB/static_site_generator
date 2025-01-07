5360bf425d70b84759dfc3ee6d1c9fd73da11a03
kingpoogie <bmadeley@getgoldn.com>
2025-01-07 17:15:13 -0500

    redoing the code. identifying bug with lists being seen as italics

--- src/code_logic.py
+++ src/code_logic.py
@@ -4,30 +4,24 @@ import re
 
 #split_nodes_delimiter: takes a list of text nodes and splits it according to the delimiter.
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    text_node_list = []
-    for node in old_nodes:
-        #print(f"The node: {node}")
-        if node.text_type == TextType.TEXT:
-            current_text = node.text
-            while True:
-                start_pos = current_text.find(delimiter)
-                if start_pos == -1:
-                    #no more delimiters, add remaining text as a node
-                    if current_text:
-                        text_node_list.append(TextNode(current_text,TextType.TEXT))
-                    break
-                end_pos = current_text.find(delimiter, start_pos+len(delimiter))              
-                if end_pos == -1:
-                    raise ValueError(f"Invalid syntax, no closing delimiter found. Delimiter: {delimiter}")
-                #add text before the delimiter
-                if start_pos > 0:
-                    text_node_list.append(TextNode(current_text[0:start_pos],TextType.TEXT,None))
-                #add delimited text
-                text_node_list.append(TextNode(current_text[start_pos+len(delimiter):end_pos],text_type))
-                current_text = current_text[end_pos + len(delimiter):]
-        else:
-            text_node_list.append(node)
-    return text_node_list
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        split_nodes = []
+        sections = old_node.text.split(delimiter)
+        if len(sections) % 2 == 0:
+            raise ValueError("Invalid markdown, formatted section not closed")
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(sections[i], TextType.TEXT))
+            else:
+                split_nodes.append(TextNode(sections[i], text_type))
+        new_nodes.extend(split_nodes)
+    return new_nodes
 
 def extract_markdown_images(text):
     #takes raw markdown text and returns a list of tuples (alt text, URL)
@@ -293,9 +287,9 @@ This is another paragraph with *italic* text and `code` here
 """
 
 lists = """
-- This is a list
-- with items
-- and *more* items
+* This is a list
+* with items
+* and *more* items
 
 1. This is an ordered list
 2. with items
